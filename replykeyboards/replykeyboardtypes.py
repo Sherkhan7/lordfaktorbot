d50e6efd15fe7a26e0b85faeab87942a5b3bd4ca
sherkhan7 <9669470@gmail.com>
2021-01-26 11:08:09 +0500

    replykeyboards added

---
+++ replykeyboards/replykeyboardtypes.py
@@ -0,0 +1,27 @@
+from replykeyboards.replykeyboardvariables import *
+
+reply_keyboard_types = {
+
+    client_menu_keyboard: {
+        "uz": {1: "Portfolio", 2: "Xizmatlarimiz", 3: "Biz bilan aloq", 4: "Biz haqimizda"},
+    },
+    admin_menu_keyboard: {
+        "uz": {1: "Yangi buyurtmalar", 2: "Qabul qilingan buyutmalar", 3: "Tarix", 4: "Sozlamalar"},
+    },
+
+    settings_keyboard: {
+        "uz": {1: "Mening ma'lumotlarim", 2: "Tilni o'zgartirish", 3: "Ortga"},
+        "cy": {1: "Менинг маълумотларим", 2: "Тилни ўзгартириш", 3: "Ортга"},
+        "ru": {1: "Мои данные", 2: "Изменить язык", 3: "Назад"},
+    },
+
+    phone_number_keyboard: {
+        "uz": {1: "Kontaktimni yuborish"},
+        "cy": {1: "Контактимни юбориш"},
+        "ru": {1: "Отправить мой контакт"}
+    },
+
+    location_keyboard: {
+        "uz": {1: 'keyingisi', 2: 'Geolokatsiyamni yuborish'}
+    }
+}
