369e6c2e0b96cc32b17212fd336b76b54d32d5c4
Sherzodbek Esanov <9669470@gmail.com>
2021-01-27 15:36:57 +0500

    Change port to 5006

--- main_lordfaktor.py
+++ main_lordfaktor.py
@@ -27,7 +27,7 @@ def main():
     # updater.start_polling()
     # updater.idle()
 
-    updater.start_webhook(listen='127.0.0.1', port=5005, url_path=TOKEN)
+    updater.start_webhook(listen='127.0.0.1', port=5006, url_path=TOKEN)
     updater.bot.set_webhook(url='https://cardel.ml/' + TOKEN)
     updater.idle()
 
